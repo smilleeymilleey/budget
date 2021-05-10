090c668f39def9e6ea8176f4f4dc2f94d2351196
Caitlyn <caitlynmiley.cm@gmail.com>
2021-05-10 08:27:24 -0500

    added a service worker file

--- server.js
+++ server.js
@@ -1,24 +1,28 @@
+  
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+const dotenv = require('dotenv');
 
-const PORT = 3000;
+dotenv.config();
 
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(logger("dev"));
-
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.set('useCreateIndex', true);
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", { 
   useNewUrlParser: true,
-  useFindAndModify: false
-});
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false });
 
 // routes
 app.use(require("./routes/api.js"));
